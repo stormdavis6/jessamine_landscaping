7fdf4f13b84690b883dd4b6ffa91a28e638920f3
Storm Davis <stormdavis6@yahoo.com>
2024-11-12 21:48:02 -0500

    move HeroSection.js image to bottom of fullscreen

--- src/app/pages/sections/HeroSection.js
+++ src/app/pages/sections/HeroSection.js
@@ -27,8 +27,12 @@ export default function HeroSection() {
     };
 
     return (
-        <section id="hero" className="bg-[#191919] text-white pt-8 md:pt-24">
-            <div className="max-w-[1920px] px-6 mx-auto flex flex-col md:flex-row md:items-center md:justify-between">
+        <section id="hero" className="bg-[#191919] text-white flex flex-col items-center pt-8 md:pt-24"
+                 style={{ minHeight: "calc(100vh - 110px)" }}  // Replace 80px with your actual navbar height
+        >
+            {/* Centered Content Wrapper */}
+            <div
+                className="flex-grow flex flex-col md:flex-row justify-center items-center max-w-[1920px] px-6 mx-auto gap-y-10 md:gap-y-0 md:gap-x-16">
 
                 {/* Text Section */}
                 <div className="flex flex-col flex-1 px-4 md:mr-32 max-w-2xl">
@@ -43,7 +47,7 @@ export default function HeroSection() {
                 </div>
 
                 {/* Buttons Section */}
-                <div className="flex flex-col px-4 md:mr-16 items-center gap-7 md:gap-9">
+                <div className="flex flex-col flex-1 px-4 md:mr-16 items-center gap-7 md:gap-9">
                     <Button
                         text="Schedule a Consultation"
                         className={"bg-[#D2D5DA] text-[#191919] hover:bg-[rgb(252,194,0)] border-none min-w-52 min-h-20 px-4 py-4 rounded-2xl cursor-pointer text-xl md:text-2xl font-bold font-figtree flex items-center justify-center"}
